fc96453260f1f89190a959e4acf9b54bd579907a
Dacidon <thremormagia@gmail.com>
2024-11-30 02:39:39 +0300

    added leaderboard + major changes

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -35,25 +35,42 @@ syava_videos = (
 @dp.message(F.dice.emoji == DiceEmoji.SLOT_MACHINE)
 async def slot_handler(message: Message):
     if not User.find(message.from_user.id):
-        User.add(message.from_user.id)
+        User.add(message.from_user.id, message.from_user.username, message.from_user.first_name)
         
     if message.dice.value in winning_nums:
-        User.reset_gamble_daily_count(message.from_user.id)
+        User.reset_gamble_lasting_count(message.from_user.id)
         User.add_to_wins_count(message.from_user.id)
-        if message.dice.value == 64 and randint(1, 2):
+        if message.dice.value == 64 and (randint(1, 2) == 1):
             await message.answer(choice(syava_videos))
         await message.reply("Победное значение, счетчик ежедневных фри спинов сброшен")
         return
     
-    User.add_to_gamble_daily_count(message.from_user.id)
+    User.add_to_gamble_lasting_count(message.from_user.id)
     User.add_to_gamble_total_count(message.from_user.id)
     
-    if User.get_gamble_daily_count(message.from_user.id) == 7:
-        User.reset_gamble_daily_count(message.from_user.id)
+    if User.get_gamble_lasting_count(message.from_user.id) == 7:
+        User.reset_gamble_lasting_count(message.from_user.id)
         await message.chat.restrict(user_id=message.from_user.id, permissions=mute_perms, until_date=dt.timedelta(minutes=5))
         await message.reply("Превышено максимальное количество фри спинов за день, пользователь замучен на 5 минут")
         return
         
-    await message.answer(f"{message.from_user.first_name}(@{message.from_user.username}) анлаки. Осталось фри спинов: {7 - User.get_gamble_daily_count(message.from_user.id)}")
+    await message.answer(f"{message.from_user.first_name} (@{message.from_user.username}) анлаки. Осталось фри спинов: {7 - User.get_gamble_lasting_count(message.from_user.id)}")
     await message.delete()
     
+@dp.message(Command("leaderboard"))
+async def leaderboard_handler(message: Message):
+    board = ""
+
+    users = User.get_all()
+
+    users.sort(key=sort_by_total_score, reverse=True)
+    
+    top10 = users[:10]
+
+    for user in top10:
+        board += f"{user[1]} (@{user[0]}):\n\tвсего: {user[2]}\n\tвыиграно:{user[3]}\n\n"
+        
+    await message.answer("ТОП-10 гэмблеров:\n\n" + board)
+        
+def sort_by_total_score(user):
+    return user[2]
--- utils/db.py
+++ utils/db.py
@@ -20,33 +20,40 @@ def check_db():
     if not os.path.exists(path):
         sql_user = """CREATE TABLE "user" (
                         "tg_id"	INTEGER NOT NULL UNIQUE,
+                        "tg_username" TEXT NOT NULL,
+                        "tg_first_name" TEXT NOT NULL,
                         "gamble_total_count" INTEGER NOT NULL DEFAULT 1,
-                        "gamble_daily_count" INTEGER NOT NULL DEFAULT 1,
+                        "gamble_lasting_count" INTEGER NOT NULL DEFAULT 1,
                         "wins_count" INTEGER NOT NULL DEFAULT 0,
                         PRIMARY KEY("tg_id")
                     )"""
         execute(sql_user)
 
 class User:
+    
+    def get_all():
+        data = execute(f"SELECT tg_username, tg_first_name, gamble_total_count, wins_count FROM user")
+        if bool(data): return data
+        else: return None
         
     def get_gamble_total_count(tg_id: int):
         data = execute(f"SELECT gamble_total_count FROM user WHERE tg_id = {tg_id}")
         if bool(data): return data[0][0]
         else: return None
         
-    def get_gamble_daily_count(tg_id: int):
-        data = execute(f"SELECT gamble_daily_count FROM user WHERE tg_id = {tg_id}")
+    def get_gamble_lasting_count(tg_id: int):
+        data = execute(f"SELECT gamble_lasting_count FROM user WHERE tg_id = {tg_id}")
         if bool(data): return data[0][0]
         else: return None
         
     def add_to_gamble_total_count(tg_id: int):
         execute(f"UPDATE user SET gamble_total_count = gamble_total_count + 1 WHERE tg_id = {tg_id}")
         
-    def add_to_gamble_daily_count(tg_id: int):
-        execute(f"UPDATE user SET gamble_daily_count = gamble_daily_count + 1 WHERE tg_id = {tg_id}")
+    def add_to_gamble_lasting_count(tg_id: int):
+        execute(f"UPDATE user SET gamble_lasting_count = gamble_lasting_count + 1 WHERE tg_id = {tg_id}")
         
-    def reset_gamble_daily_count(tg_id: int):
-        execute(f"UPDATE user SET gamble_daily_count = 0 WHERE tg_id = {tg_id}")
+    def reset_gamble_lasting_count(tg_id: int):
+        execute(f"UPDATE user SET gamble_lasting_count = 0 WHERE tg_id = {tg_id}")
         
     def add_to_wins_count(tg_id: int):
         execute(f"UPDATE user SET wins_count = wins_count + 1 WHERE tg_id = {tg_id}")
@@ -55,8 +62,8 @@ class User:
         data = execute(f"SELECT * FROM user WHERE tg_id = {tg_id}")
         return bool(data)
     
-    def add(tg_id: int):
-        execute(f"INSERT INTO user (tg_id) VALUES ({tg_id})")
+    def add(tg_id: int, tg_username: str, tg_first_name: str):
+        execute(f"INSERT INTO user (tg_id, tg_username, tg_first_name) VALUES ({tg_id}, '{tg_username}', '{tg_first_name}')")
         
     def delete(tg_id: int):
         execute(f"DELETE FROM user WHERE tg_id = {tg_id}")
