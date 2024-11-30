86ccd1259391946c12fe161067b156c6079468c0
dacidon <thremormagia@gmail.com>
2024-11-30 16:03:22 +0300

    added delays and message deleting + changed wording for messages

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,4 +1,4 @@
-from utils.loader import dp
+from utils.loader import dp, bot
 from utils.db import User
 
 import datetime as dt
@@ -6,9 +6,11 @@ from aiogram.types.message import Message
 from aiogram.types.dice import DiceEmoji
 from aiogram.types.chat_permissions import ChatPermissions
 from aiogram.filters.command import Command
+from aiogram.methods.delete_message import DeleteMessage
 from aiogram import F
-from random import randint, choice
+from random import randint, choice, random
 from emoji import *
+from asyncio import sleep
 
 winning_nums = (1, 22, 43, 64)
 
@@ -36,12 +38,17 @@ syava_videos = (
 @dp.message(F.dice.emoji == DiceEmoji.SLOT_MACHINE)
 async def slot_handler(message: Message):
     if not User.find(message.from_user.id):
+        if message.from_user.username == None:
+            User.add(message.from_user.id, message.from_user.first_name, message.from_user.first_name)
+            return
         User.add(message.from_user.id, message.from_user.username, message.from_user.first_name)
         
     if message.dice.value in winning_nums:
+        rand = randint(1, 2)
         User.reset_gamble_lasting_count(message.from_user.id)
         User.add_to_wins_count(message.from_user.id)
-        if message.dice.value == 64 and (randint(1, 2) == 1):
+        await sleep(2)
+        if message.dice.value == 64 and rand == 1:
             await message.answer(choice(syava_videos))
         await message.reply(emojize(":party_popper:Виннер-виннер чикен диннер, счетчик фри спинов сброшен"))
         return
@@ -52,11 +59,17 @@ async def slot_handler(message: Message):
     if User.get_gamble_lasting_count(message.from_user.id) == 7:
         User.reset_gamble_lasting_count(message.from_user.id)
         await message.chat.restrict(user_id=message.from_user.id, permissions=mute_perms, until_date=dt.timedelta(minutes=1))
-        await message.reply(emojize(":no_entry:Превышено максимальное количество фри спинов, пользователь замучен на 1 минуту"))
+        await sleep(2)
+        await message.reply(emojize(":no_entry:Истрачены все спины, пользователь замучен на 1 минуту"))
         return
         
-    await message.answer(emojize(f":slightly_frowning_face:{message.from_user.first_name} (@{message.from_user.username}) анлаки\nОсталось фри спинов: {7 - User.get_gamble_lasting_count(message.from_user.id)}"))
+    msg_count = 7 - User.get_gamble_lasting_count(message.from_user.id)
+    
+    await sleep(2)
+    msg = await message.answer(emojize(f":slightly_frowning_face:{message.from_user.first_name} анлаки\nОсталось спинов: {msg_count}"))
     await message.delete()
+    await sleep(2)
+    await bot(DeleteMessage(chat_id=message.chat.id, message_id=msg.message_id))
     
 @dp.message(Command("leaderboard"))
 async def leaderboard_handler(message: Message):
@@ -69,7 +82,7 @@ async def leaderboard_handler(message: Message):
     top10 = users[:10]
 
     for user in top10:
-        board += emojize(f"{user[1]} (@{user[0]}):\n\t\t\t:input_numbers:Всего: {user[2]}\n\t\t\t:party_popper:Выиграно:{user[3]}\n\n")
+        board += emojize(f"{user[1]} ({user[0]}):\n\t\t\t:input_numbers:Всего: {user[2]}\n\t\t\t:party_popper:Выиграно: {user[3]}\n\n")
         
     await message.answer(emojize(":TOP_arrow:ТОП-10 гэмблеров:\n\n" + board))
         
